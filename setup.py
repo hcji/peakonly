bd3d4c4f12a82a6aa359ca930f5e23320ec092a8
Hongchao Ji <ji.hongchao@foxmail.com>
2022-08-17 14:09:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
+    name='peakonly',
+    packages=find_packages(),
     ext_modules=cythonize('cython_utils/roi.pyx'),
     include_dirs=[np.get_include()]
 )
